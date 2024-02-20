872db8639441ded83cf0e64044622ddbf9279ae2
joaovmoura <joao.vitor.figueiredo@ccc.ufcg.edu.br>
2024-02-19 23:07:15 -0300

    update peça de dama

--- src/Dama/PecaDama.java
+++ src/Dama/PecaDama.java
@@ -1,4 +1,13 @@
 package Dama;
 
 public class PecaDama {
-}
+
+    private Integer posX;
+    private Integer posY;
+
+    PecaDama(int x, int y){
+        this.posX = x;
+        this.posY = y;
+    }
+    
+    }
